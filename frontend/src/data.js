edf497e2e1d18f3dae7f0b286e3c189a6c0a124d
Shubham Hetta <shubham.hetta95@gmail.com>
2021-05-28 21:50:28 +0530

    share code on github

---
+++ frontend/src/data.js
@@ -0,0 +1,78 @@
+const data = {
+    products :[
+        {
+            _id:'1',
+            name:'Wooden Dog Heart',
+            category:'Wood',
+            image:'/images/p1.jpg',
+            price:250,
+            brand:'Shubham Hetta',
+            rating:4.5,
+            numReviews:10,
+            description:'high quality product',
+
+        },
+        {
+            _id:'2',
+            name:'Wooden Welcome Wall Art',
+            category:'Wood',
+            image:'/images/p2.jpg',
+            price:1000,
+            brand:'Shubham Hetta',
+            rating:4.5,
+            numReviews:10,
+            description:'high quality product',
+
+        },
+        {
+            _id:'3',
+            name:'Wooden Trishul',
+            category:'Wood',
+            image:'/images/p3.jpg',
+            price:2000,
+            brand:'Shubham Hetta',
+            rating:4.5,
+            numReviews:10,
+            description:'high quality product',
+
+        },
+        {
+            _id:'4',
+            name:'Wooden Dog Heart',
+            category:'Wood',
+            image:'/images/p1.jpg',
+            price:250,
+            brand:'Shubham Hetta',
+            rating:4.5,
+            numReviews:10,
+            description:'high quality product',
+
+        },
+        {
+            _id:'5',
+            name:'Wooden Dog Heart',
+            category:'Wood',
+            image:'/images/p1.jpg',
+            price:250,
+            brand:'Shubham Hetta',
+            rating:4.5,
+            numReviews:10,
+            description:'high quality product',
+
+        },
+        {
+            _id:'6',
+            name:'Wooden Dog Heart',
+            category:'Wood',
+            image:'/images/p1.jpg',
+            price:250,
+            brand:'Shubham Hetta',
+            rating:4.5,
+            numReviews:10,
+            description:'high quality product',
+
+        },
+        
+    ],
+};
+export default data;
